8ed516daed75a08c6bb9147e7fd49a8b068c5e8e
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-03-07 17:13:52 +0200

    Added comments

--- src/background.js
+++ src/background.js
@@ -1,16 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => { // Once the extension is created, the context menu is created.
     chrome.contextMenus.create({
-        "id": "rotateImage",
-        "title": "Rotate Image",
-        "contexts": ["image"]
+        "id": "rotateImage", // id of the context menu
+        "title": "Rotate Image", // what will be written in the context menu
+        "contexts": ["image"] // on which items the right-click will show this context menu
     })
 });
 
 try{
-    chrome.contextMenus.onClicked.addListener((info, tab) => {
-        if(info.menuItemId === "rotateImage"){
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {
+    chrome.contextMenus.onClicked.addListener((info, tab) => { // once the item in the context menu this waits for the user to click it
+        if(info.menuItemId === "rotateImage"){ // if the user clicked on the correct item
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) { // it takes the active tab
+                chrome.tabs.sendMessage(tabs[0].id, { // sends a message with the tab id and the "rotateImage" type
                     type: "rotateImage",
                 });
             });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,15 +1,15 @@
 let rotation = 90;
 
 function rotateImage() {
-  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)';
-  rotation += 90;
+  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)'; // getElementsByTageName - because there is no id tag
+  rotation += 90; // everytime the button is clicked it rotates one more time.
   if (rotation >= 360) {
   	rotation = 0;
   }
 };
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-	if (msg.type === 'rotateImage') {
-		rotateImage();
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) { // this waits for any message 
+	if (msg.type === 'rotateImage') { // onces there is a message with a type of "rotateImage"
+		rotateImage(); // it activates the function
 	}
 });
