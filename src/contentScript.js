32f3ab788045b0cfe861169a5eee9a9065b0e1f3
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-03-07 15:49:16 +0200

    Added more rotation options

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,11 @@
+let rotation = 90;
+
 function rotateImage() {
-  document.getElementsByTagName("img")[0].style.transform = "rotate(90deg)";
+  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)';
+  rotation += 90;
+  if (rotation >= 360) {
+  	rotation = 0;
+  }
 };
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
