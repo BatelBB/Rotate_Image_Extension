91f00b35bbd469b760143b0915c8ea2034f3e7a8
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-03-11 15:01:11 +0200

    fixed error and added zoom-out option

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,15 +1,24 @@
-let rotation = 90;
+let rotation = 0;
+
 
 function rotateImage() {
-  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)'; // getElementsByTageName - because there is no id tag
+  if(document.getElementsByTagName("img")[0].style.cursor == "zoom-out"){ // in case the image is zoomed in
+    rotation = 0;
+  }
+
   rotation += 90; // everytime the button is clicked it rotates one more time.
+  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)'; // getElementsByTageName - because there is no id tag
   if (rotation >= 360) {
   	rotation = 0;
   }
+  
 };
+
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) { // this waits for any message 
 	if (msg.type === 'rotateImage') { // onces there is a message with a type of "rotateImage"
 		rotateImage(); // it activates the function
 	}
+Promise.resolve("").then(result => sendResponse(result)); // to close the port of the listener
+return true;
 });
