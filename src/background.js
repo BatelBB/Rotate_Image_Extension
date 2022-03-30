d56d5901d0ddb49b4650d706ca24fa7b406827a2
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-03-30 21:20:36 +0300

    Added submenu options for clock and anti clock wise

--- src/background.js
+++ src/background.js
@@ -1,17 +1,39 @@
 chrome.runtime.onInstalled.addListener(() => { // Once the extension is created, the context menu is created.
+  
     chrome.contextMenus.create({
-        "id": "rotateImage", // id of the context menu
+        "id": "parent", // id of the context menu
         "title": "Rotate Image", // what will be written in the context menu
         "contexts": ["image"] // on which items the right-click will show this context menu
-    })
+    });
+
+    chrome.contextMenus.create({
+        "id": "clock-wise",
+        "title": "Rotate Clock-Wise",
+        "parentId": "parent",
+        "contexts": ["image"]
+    });
+
+    chrome.contextMenus.create({
+        "id": "anti-clock-wise",
+        "title": "Rotate Anti-Clock-Wise",
+        "parentId": "parent",
+        "contexts": ["image"]
+    }); 
 });
 
 try{
     chrome.contextMenus.onClicked.addListener((info, tab) => { // once the item in the context menu this waits for the user to click it
-        if(info.menuItemId === "rotateImage"){ // if the user clicked on the correct item
+        if(info.menuItemId === "clock-wise"){ // if the user clicked on the correct item
             chrome.tabs.query({active: true, currentWindow: true}, function (tabs) { // it takes the active tab
                 chrome.tabs.sendMessage(tabs[0].id, { // sends a message with the tab id and the "rotateImage" type
-                    type: "rotateImage",
+                    type: "rotateImageClockWise",
+                });
+            });
+        }
+        if(info.menuItemId === "anti-clock-wise"){ // if the user clicked on the correct item
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) { // it takes the active tab
+                chrome.tabs.sendMessage(tabs[0].id, { // sends a message with the tab id and the "rotateImage" type
+                    type: "rotateImageAntiClockWise",
                 });
             });
         }
@@ -19,6 +41,7 @@ try{
 } catch (e) {
     console.error(e);
 }
+
 
 if (chrome.runtime) {
     chrome.runtime.setUninstallURL("https://forms.gle/K1v6S13SnPKzsE2b7"); // a google form that opens if the user uninstalls the extension
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,7 @@
 let rotation = 0;
 
 
-function rotateImage() {
+function rotateImageClockWise() {
   if(document.getElementsByTagName("img")[0].style.cursor == "zoom-out"){ // in case the image is zoomed in
     rotation = 0;
   }
@@ -14,10 +14,25 @@ function rotateImage() {
   
 };
 
+function rotateImageAntiClockWise() {
+  if(document.getElementsByTagName("img")[0].style.cursor == "zoom-out"){ // in case the image is zoomed in
+    rotation = 0;
+  }
+
+  rotation -= 90; // everytime the button is clicked it rotates one more time.
+  document.getElementsByTagName("img")[0].style.transform = 'rotate(' + rotation + 'deg)'; // getElementsByTageName - because there is no id tag
+  if (rotation <= -360) {
+  	rotation = 0;
+  }
+  
+};
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) { // this waits for any message 
-	if (msg.type === 'rotateImage') { // onces there is a message with a type of "rotateImage"
-		rotateImage(); // it activates the function
+	if (msg.type === 'rotateImageClockWise') { // onces there is a message with a type of "rotateImageClockWise"
+		rotateImageClockWise(); // it activates the function
+	}
+  if (msg.type === 'rotateImageAntiClockWise') { // onces there is a message with a type of "rotateImageAntiClockWise"
+		rotateImageAntiClockWise(); // it activates the function
 	}
 Promise.resolve("").then(result => sendResponse(result)); // to close the port of the listener
 return true;
